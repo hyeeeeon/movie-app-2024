5afffb487608462942d7d84fc360f4f54fc5fd37
seung hyeon <shyeeoon@gmail.com>
2025-09-26 22:19:22 +0900

    올바른 파일로 내용 업데이트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route, Routes} from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Navigation from "./components/Navigation";
@@ -7,9 +7,10 @@ import Navigation from "./components/Navigation";
 function App(){
   return (
     <HashRouter>
+      <Navigation />
       <Routes>
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </HashRouter>
   );
