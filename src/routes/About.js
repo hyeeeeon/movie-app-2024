4747e20dc1e24e383c7053b8a6aefd9850648e0f
seung hyeon <shyeeoon@gmail.com>
2025-09-26 22:07:27 +0900

    올바른 파일로 내용 업데이트

---
+++ src/routes/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./About.css";
+
+function About(){
+    return(
+        <div className="about__container">
+            <span>
+                "Freedom is the freedom to say that two plus two make four.
+                If that is granted, all else follows. "
+            </span>
+            <span>-Georage Orwell, 1984</span>
+        </div>
+    )
+    return 
+        <span>About this page: I built it beacuse I live movies. </span>
+    
+}
+
+export default About;
